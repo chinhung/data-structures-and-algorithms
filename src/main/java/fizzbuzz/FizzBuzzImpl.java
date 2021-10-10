c3540d06bca08d3e0283b807c1ad4ecb9734cc8d
chinhung <chinhung101@gmail.com>
2021-10-10 17:56:28 +0800

    Add FizzBuzz implementation

---
+++ src/main/java/fizzbuzz/FizzBuzzImpl.java
@@ -0,0 +1,17 @@
+package fizzbuzz;
+
+public class FizzBuzzImpl implements FizzBizz {
+
+    @Override
+    public String execute(int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(number);
+        }
+    }
+}

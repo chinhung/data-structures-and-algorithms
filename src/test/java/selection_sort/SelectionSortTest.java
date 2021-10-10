8e8bb6221041400966002fa335db92dfa82e5d16
chinhung <chinhung101@gmail.com>
2021-10-10 19:12:46 +0800

    Add SelectionSort test case

---
+++ src/test/java/selection_sort/SelectionSortTest.java
@@ -0,0 +1,27 @@
+package selection_sort;
+
+import org.junit.jupiter.api.Test;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SelectionSortTest {
+
+    public Integer[] getUnsorted() {
+        return new Integer[]{36, -55, 0, -1, 80, -17, 1};
+    }
+
+    public Integer[] getSorted() {
+        return new Integer[]{-55, -17, -1, 0, 1, 36, 80};
+    }
+
+    @Test
+    public void testSelectionSort() {
+        SelectionSort<Integer> selectionSort = new SelectionSortImpl<>();
+        Comparator<Integer> naturalOrder = Comparator.comparingInt((Integer o) -> o);
+
+        Integer[] actual = selectionSort.sort(getUnsorted(), naturalOrder);
+
+        assertArrayEquals(getSorted(), actual);
+    }
+}

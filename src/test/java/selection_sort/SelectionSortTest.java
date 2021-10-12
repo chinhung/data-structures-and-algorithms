9a0d414d721f6d3138688d40e4596a3a5801fbed
chinhung <chinhung101@gmail.com>
2021-10-12 21:23:46 +0800

    Extract TestData class

--- src/test/java/selection_sort/SelectionSortTest.java
+++ src/test/java/selection_sort/SelectionSortTest.java
@@ -1,29 +1,23 @@
 package selection_sort;
 
 import org.junit.jupiter.api.Test;
+import test_util.TestData;
+
 import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SelectionSortTest {
 
-    public Integer[] getUnsorted() {
-        return new Integer[]{36, -55, 0, -1, 80, -17, 1};
-    }
-
-    public Integer[] getSorted() {
-        return new Integer[]{-55, -17, -1, 0, 1, 36, 80};
-    }
-
     @Test
     public void testSelectionSort() {
         SelectionSort<Integer> selectionSort = new SelectionSortImpl<>();
         Comparator<Integer> naturalOrder = Comparator.comparingInt((Integer o) -> o);
 
-        Integer[] unsorted = getUnsorted();
+        Integer[] unsorted = TestData.getUnsorted();
         selectionSort.sort(unsorted, naturalOrder);
         Integer[] actual = unsorted;
 
-        assertArrayEquals(getSorted(), actual);
+        assertArrayEquals(TestData.getSorted(), actual);
     }
 }
---
+++ src/test/java/test_util/TestData.java
@@ -0,0 +1,12 @@
+package test_util;
+
+public class TestData {
+
+    public static Integer[] getUnsorted() {
+        return new Integer[]{36, -55, 0, -1, 80, -17, 1};
+    }
+
+    public static Integer[] getSorted() {
+        return new Integer[]{-55, -17, -1, 0, 1, 36, 80};
+    }
+}

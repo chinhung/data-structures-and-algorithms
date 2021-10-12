fe0fea7e35cc024943b934b02a527791b19a9153
chinhung <chinhung101@gmail.com>
2021-10-12 23:07:58 +0800

    Modify Fibonacci test cases

--- src/test/java/fibonacci/FibonacciTest.java
+++ src/test/java/fibonacci/FibonacciTest.java
@@ -18,7 +18,7 @@ public class FibonacciTest {
         assertEquals(5L, fibonacci.calculate(5));
         assertEquals(8L, fibonacci.calculate(6));
 
-        assertEquals(1134903170L, fibonacci.calculate(45));
+        assertEquals(267914296L, fibonacci.calculate(42));
     }
 
     @Test
@@ -33,7 +33,7 @@ public class FibonacciTest {
         assertEquals(5L, fibonacci.calculate(5));
         assertEquals(8L, fibonacci.calculate(6));
 
-        assertEquals(1134903170L, fibonacci.calculate(45));
+        assertEquals(267914296L, fibonacci.calculate(42));
     }
 
     @Test
@@ -48,6 +48,6 @@ public class FibonacciTest {
         assertEquals(5L, fibonacci.calculate(5));
         assertEquals(8L, fibonacci.calculate(6));
 
-        assertEquals(1134903170L, fibonacci.calculate(45));
+        assertEquals(267914296L, fibonacci.calculate(42));
     }
 }

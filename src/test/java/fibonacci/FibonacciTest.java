bc6fe5fdb26f7fd668e5b44703faedfd2066fc12
chinhung <chinhung101@gmail.com>
2021-10-12 22:04:27 +0800

    Add RecursiveFibonacci test case

--- src/test/java/fibonacci/FibonacciTest.java
+++ src/test/java/fibonacci/FibonacciTest.java
@@ -7,8 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FibonacciTest {
 
     @Test
-    public void testFibonacciCalculate() {
+    public void testArrayFibonacci() {
         Fibonacci fibonacci = new ArrayFibonacci();
+
+        assertEquals(0L, fibonacci.calculate(0));
+        assertEquals(1L, fibonacci.calculate(1));
+        assertEquals(1L, fibonacci.calculate(2));
+        assertEquals(2L, fibonacci.calculate(3));
+        assertEquals(3L, fibonacci.calculate(4));
+        assertEquals(5L, fibonacci.calculate(5));
+        assertEquals(8L, fibonacci.calculate(6));
+
+        assertEquals(12586269025L, fibonacci.calculate(50));
+    }
+
+    @Test
+    public void testRecursiveFibonacci() {
+        Fibonacci fibonacci = new RecursiveFibonacci();
 
         assertEquals(0L, fibonacci.calculate(0));
         assertEquals(1L, fibonacci.calculate(1));
